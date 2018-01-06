105eb02d46af8872bb01a68d823385e6d100a1aa
prathmesh-dali <prathmesh.dali@gmail.com>
2018-01-06 21:04:09 +0530

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  data= []
-  constructor(){
+  data= [];
+  constructor() {
     for (let i = 0; i < 6400; i++) {
       this.data.push({
-        Action: 'Add',
-        ChangeTrackingId: '285497',
-        EffectiveDate: '9/13/2017 12:00:00 AM',
-        EndDate: '9/13/9999 12:00:00 AM',
+        Action: i % 3 ? 'Remove' : 'Add',
+        ChangeTrackingId: '28549' + i,
+        EffectiveDate: '9/' + (i % 10 + 1) + '/2017 12:00:00 AM',
+        EndDate: '9/' + (i % 10 + 1) + '/9999 12:00:00 AM',
         FromIACode: 'test' + i % 10,
         FromIACodeFees: null,
         FromIACodeName: null,
@@ -25,10 +25,10 @@ export class AppComponent {
         ToIACode: 'test' + i % 20,
         ToIACodeFees: null,
         ToIACodeName: null,
-        ValidationStatus: 'Failed',
+        ValidationStatus: i % 3 ? 'Failed' : 'Passed',
         Value: null,
         comment: null,
-        _id: '3435345u230532fsfs9u504'+i
+        _id: '3435345u230532fsfs9u504' + i
       });
     }
   }
